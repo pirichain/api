8b05d59dec14321140fcd634c6ce135aba0fdc69
musatotan <musatotan@gmail.com>
2023-07-13 22:34:16 +0300

    crypto operations delayed for next update until find another way to done

--- src/wallet/index.js
+++ src/wallet/index.js
@@ -1,12 +1,20 @@
-const {createNewAddress} = require("./createNewAddress");
-const {getMnemonic} = require("./getMnemonic");
-const {rescuePrivateKey} = require("./rescuePrivateKey");
+// //TO-DO: update with crypto browser conf.
+// const {createNewAddress} = require("./createNewAddress");
+// const {getMnemonic} = require("./getMnemonic");
+// const {rescuePrivateKey} = require("./rescuePrivateKey");
 
 class Wallet {
     constructor(client) {
         this.client = client;
     }
-    createNewAddress = async () => createNewAddress();
+    //TO-DO: update with crypto browser conf.
+    // createNewAddress = async () => createNewAddress();
+    createNewAddress(language = 'english'){
+        return this.client.post("/createNewAddress", {
+            language: language
+        })
+    }
+
     getBalance(address, assetID) {
         return this.client.post("/getBalance", {
             'address': address,
@@ -18,9 +26,23 @@ class Wallet {
             "address": address
         })
     }
-    getMnemonic = (privateKey, language = 'english') => getMnemonic(privateKey, language);
+    //TO-DO: update with crypto browser conf.
+    // getMnemonic = (privateKey, language = 'english') => getMnemonic(privateKey, language);
+    getMnemonic(privateKey, language = 'english'){
+        return this.client.post("/getMnemonic", {
+            privateKey: privateKey,
+            language: language
+        })
+    }
 
-    rescuePrivateKey = async (words, language = 'english') => rescuePrivateKey(words, language)
+    //TO-DO: update with crypto browser conf.
+    // rescuePrivateKey = async (words, language = 'english') => rescuePrivateKey(words, language)
+    rescuePrivateKey(words, language = 'english'){
+        return this.client.post("/getMnemonic", {
+            words: words,
+            language: language
+        })
+    }
 
 }
 
