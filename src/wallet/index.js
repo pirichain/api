b9eccd9d4006f0026c64e2faafd4f087f0220030
musatotan <musatotan@gmail.com>
2023-09-05 14:35:45 +0300

    wallet operations has been moved to offline.

--- src/wallet/index.js
+++ src/wallet/index.js
@@ -1,20 +1,12 @@
-// //TO-DO: update with crypto browser conf.
-// const {createNewAddress} = require("./createNewAddress");
-// const {getMnemonic} = require("./getMnemonic");
-// const {rescuePrivateKey} = require("./rescuePrivateKey");
+const {createNewAddress} = require("./createNewAddress");
+const {getMnemonic} = require("./getMnemonic");
+const {rescuePrivateKey} = require("./rescuePrivateKey");
 
 class Wallet {
     constructor(client) {
         this.client = client;
     }
-    //TO-DO: update with crypto browser conf.
-    // createNewAddress = async () => createNewAddress();
-    createNewAddress(language = 'english'){
-        return this.client.post("/createNewAddress", {
-            language: language
-        })
-    }
-
+    createNewAddress = async () => createNewAddress();
     getBalance(address, assetID) {
         return this.client.post("/getBalance", {
             'address': address,
@@ -26,24 +18,8 @@ class Wallet {
             "address": address
         })
     }
-    //TO-DO: update with crypto browser conf.
-    // getMnemonic = (privateKey, language = 'english') => getMnemonic(privateKey, language);
-    getMnemonic(privateKey, language = 'english'){
-        return this.client.post("/getMnemonic", {
-            privateKey: privateKey,
-            language: language
-        })
-    }
-
-    //TO-DO: update with crypto browser conf.
-    // rescuePrivateKey = async (words, language = 'english') => rescuePrivateKey(words, language)
-    rescuePrivateKey(words, language = 'english'){
-        return this.client.post("/rescuePrivateKey", {
-            words: words,
-            language: language
-        })
-    }
-
+    getMnemonic = (privateKey, language = 'english') => getMnemonic(privateKey, language);
+    rescuePrivateKey = async (words, language = 'english') => rescuePrivateKey(words, language)
 }
 
 module.exports = Wallet;
